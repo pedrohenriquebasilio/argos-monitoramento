b0b406098de7f432fdc47f9954d60b7afeeca5e6
Pedro <phbasilio6903@gmail.com>
2025-09-05 09:24:37 -0300

    initial commit

---
+++ src/main/java/argos/com/br/crud_basico/CrudBasicoApplication.java
@@ -0,0 +1,13 @@
+package argos.com.br.crud_basico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBasicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/argos/com/br/crud_basico/CrudBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package argos.com.br.crud_basico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
