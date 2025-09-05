70b4788126ed3565fdf35ca58a424796555db4f0
Pedro <phbasilio6903@gmail.com>
2025-09-05 09:56:28 -0300

    feat: add entity Comment

---
+++ src/main/java/argos/com/br/crud_basico/domain/model/Comment.java
@@ -0,0 +1,29 @@
+package argos.com.br.crud_basico.domain.model;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
