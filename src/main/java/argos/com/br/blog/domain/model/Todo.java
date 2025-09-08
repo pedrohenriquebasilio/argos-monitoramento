2787ae119544b5c80858f42c4b48329b6af6f068
Pedro <phbasilio6903@gmail.com>
2025-09-08 15:52:56 -0300

    feat: adicionei a entidade Todo com atributos e relacionamentos

---
+++ src/main/java/argos/com/br/blog/domain/model/Todo.java
@@ -0,0 +1,34 @@
+package argos.com.br.blog.domain.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private boolean completed = false;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
