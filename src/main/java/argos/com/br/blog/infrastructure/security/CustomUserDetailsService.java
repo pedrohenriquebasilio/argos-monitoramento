367e58d7d6b078fbc504010fe676a7a806d95cf2
Pedro <phbasilio6903@gmail.com>
2025-09-08 15:54:00 -0300

    feat: implementada a classe CustomUserDetailsService para carregar usuários por email

---
+++ src/main/java/argos/com/br/blog/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package argos.com.br.blog.infrastructure.security;
+
+
+import argos.com.br.blog.domain.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserRepository users;
+
+    public CustomUserDetailsService(UserRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var u = users.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(u.getEmail())
+                .password(u.getPassword())
+                .authorities("ROLE_USER")
+                .build();
+    }
+}
+
